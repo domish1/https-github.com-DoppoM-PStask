4855716d275ee73164035957c8e6bf9ff488188e
DoppoM <36840055+Hichner@users.noreply.github.com>
2018-11-07 17:43:42 +0300

    Initial commit

---
+++ 001.ps1
@@ -0,0 +1,6 @@
+﻿$var= "asdsafwq Hello World qweqw Hello World asdqwrqrq Hello World"
+$var= $var -replace "Hello World", "Happy New Year"
+$i=($var.Split(" ") | Select-string "New").Count
+$var = $var+" Result: "+ $i
+$j=($var.Split(" ") | Select-string "$").Count
+if($var -notmatch "Hello World"){$var|Out-File G:\1\$j.txt}
